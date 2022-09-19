40df97c8b37af90e42fe14c58ac644281a7669fe
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2022-09-19 10:37:17 -0700

    added functions to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,26 @@
+
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log ('Please Try again!');
+    }
+}
 
 /*
 if (topic === 'HTML') {
@@ -12,15 +34,19 @@ if (topic === 'HTML') {
 } else {
     console.log('Please try again!');
 }
-*/
 
-for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
-}
-
-/*
 var shapes = ["triangle", "sqaure", "pentagon", "circle"];
 for (var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
 }
 */
+
+function helloWorld() {
+    console.log("Hello, world. I am a function!")
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
